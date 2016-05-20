6af0e51d660a85c690506c0b135cc09fcc3a6bef
OleksiiRudis <rudis@ua.fm>
2016-05-20 17:41:00 +0300

    Update main.py

--- New Project-20160520/main.py
+++ New Project-20160520/main.py
@@ -1,3 +1,17 @@
 # Hello World program in Python
+
+def sklonenie(n, args):
+    if args is None:
+        return False
+    arg_list = [arg.strip() for arg in args.split(',')]
+    if n%10 == 1 and n%100 != 11:
+        str = arg_list[0]
+    elif n%10 >= 2 and n%10 <= 4 and (n%100 < 10 or n%100 >= 20):
+        str = arg_list[1]
+    else:
+        str = arg_list[2]
+    return str
+
+for num in range(1,150): 
+    print num, " ", sklonenie(num, "tovar,tovara,tovarov")
     
-print "Hello World!\n"
